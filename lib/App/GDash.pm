fcf7b42cd72449943f6d93ee5f946a007ce97c48
Gene Boggs <gene@ology.net>
2022-12-27 08:58:46 -0800

    Fix name

--- lib/App/GDash.pm
+++ lib/App/GDash.pm
@@ -11,8 +11,8 @@ sub startup ($self) {
   my $r = $self->routes;
   $r->get('/')       ->to('Main#index')   ->name('index');
   $r->post('/')      ->to('Main#update')  ->name('update');
+  $r->get('/refresh')->to('Main#refresh') ->name('refresh');
   $r->get('/delete') ->to('Main#delete')  ->name('delete');
-  $r->get('/refresh')->to('Main#refresh') ->name('delete');
   $r->post('/new')   ->to('Main#new_card')->name('new');
   $r->get('/help')   ->to('Main#help')    ->name('help');
 }
