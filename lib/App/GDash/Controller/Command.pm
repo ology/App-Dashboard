f635303e5a19e3a913762d6d02f372fd9739ef18
Gene Boggs <gene@ology.net>
2022-12-30 11:28:53 -0800

    WIP: Move command functionality from Main

---
+++ lib/App/GDash/Controller/Command.pm
@@ -0,0 +1,67 @@
+package App::GDash::Controller::Command;
+
+use Capture::Tiny qw(capture);
+use Encoding::FixLatin qw(fix_latin);
+use HTTP::Simple qw(getstore);
+use Mojo::DOM ();
+use XML::RSS ();
+
+sub rss {
+  my ($feed) = @_;
+  my $content = '';
+  my $rss_content = 'rss-content.xml';
+  unlink $rss_content;
+  _get_file($feed, $rss_content);
+  if (-e $rss_content) {
+    my $rss = XML::RSS->new;
+    eval { $rss->parsefile($rss_content) };
+    if ($@) {
+      warn "Can't parse $rss_content: $@\n";
+    }
+    else {
+      $content = '<ul>';
+      my $n = 0;
+      for my $item ($rss->{items}->@*) {
+        $n++;
+        my $text = $item->{title};
+        unless ($text) {
+          my $dom = Mojo::DOM->new($item->{description});
+          $text = $dom->all_text;
+          $text = substr($text, 0, 49) . '...';
+        }
+        $content .= qq|<li><a href="$item->{link}" target="_blank">$text</a></li>|;
+        last if $n >= 20;
+      }
+      $content .= '</ul>';
+    }
+  }
+  return $content;
+}
+
+sub perl {
+  my ($code) = @_;
+  my $command = "perl -Mojo -E'$code'";
+  my $content = '';
+  return 'Invalid' if $command =~ /\bsystem\b/;
+  my ($stdout, $stderr) = capture { system($command) };
+  chomp $stdout;
+  return $stderr ? $stderr : $stdout;
+}
+
+sub curl {
+  my ($code) = @_;
+  my $command = "curl $code";
+  my ($stdout) = capture { system($command) };
+  chomp $stdout;
+  return fix_latin($stdout);
+}
+
+sub _get_file {
+    my ($url, $file) = @_;
+    my $status = getstore($url, $file)
+        or warn "Can't getstore $url to $file: $!\n";
+    warn "Could not get $file from $url\n"
+        unless $status == 200;
+}
+
+1;
