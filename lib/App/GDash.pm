8ba93591c13a063bc79337d2421ab085080df7d1
Gene Boggs <gene@ology.net>
2023-01-05 07:21:30 -0800

    Add /swap

--- lib/App/GDash.pm
+++ lib/App/GDash.pm
@@ -11,6 +11,7 @@ sub startup ($self) {
   my $r = $self->routes;
   $r->get('/')       ->to('Main#index')   ->name('index');
   $r->post('/')      ->to('Main#update')  ->name('update');
+  $r->get('/swap')   ->to('Main#swap')    ->name('swap');
   $r->get('/refresh')->to('Main#refresh') ->name('refresh');
   $r->get('/delete') ->to('Main#delete')  ->name('delete');
   $r->post('/new')   ->to('Main#new_card')->name('new');
